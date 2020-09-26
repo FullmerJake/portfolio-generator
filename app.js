bc7247bbd7f5f1abf90e0bf7936e4f60ce41122d
Jake Fullmer <w.jake.fullmer@gmail.com>
2020-09-26 02:19:42 -0600

    added inquirer to app

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-const profileDataArgs = process.argv.slice(2);
+// const pageHTML = generatePage(name, github);
 
-// assignment deconstructing. assigns the elements we get from user input, and assigns them to name, and github respectively. 
-const [name, github] = profileDataArgs;
+// // assignment deconstructing. assigns the elements we get from user input, and assigns them to name, and github respectively. 
+// // const [name, github] = profileDataArgs;
 
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw new Error(err); 
+// fs.writeFile('./index.html', pageHTML, err => {
+//     if (err) throw err; 
     
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));

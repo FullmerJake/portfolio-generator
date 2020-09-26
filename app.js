5975929a1127edd48d36afe330403590dd158311
Jake Fullmer <w.jake.fullmer@gmail.com>
2020-09-26 02:50:49 -0600

    added more questions, and the abilitiy to add new projects as well

--- app.js
+++ app.js
@@ -14,12 +14,89 @@ const inquirer = require('inquirer');
 //     console.log('Portfolio complete! Check out index.html to see the output!');
 // });
 
-inquirer
-    .prompt([
+
+const promptUser = () => {
+    return inquirer.prompt([
         {
             type: 'input',
             name: 'name',
-            message: 'What is your name?'
+            messgae: 'What is your name?'
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Enter your Github Username'
+        },
+        {
+            type: 'input',
+            name: 'about',
+            message: 'Provide some information about yourself'
         }
     ])
-    .then(answers => console.log(answers));
+};
+
+//saves inquirer questions and answers in a function so it can be called whenever you feel like it. 
+const promptProject = portfolioData => {
+
+    if (!portfolioData.projects) {
+        portfolioData.projects = [];
+    }
+
+    console.log(`
+    ==================
+    Add a New Project
+    ==================
+    `);
+    // uses inquirer functionality to create question, and save user input answers. 
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name:'name',
+            message: 'What is the name of your project?'
+        },
+        {
+            type: 'input', 
+            name: 'description', 
+            message: 'Provide a description of the project'
+        },
+        {
+            type: 'checkbox',
+            name: 'languages',
+            message: 'What did you build this project with? (Check all that apply)',
+            choices: ['Javascript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+        },
+        {
+            type: 'input', 
+            name: 'link',
+            message: 'Enter the Github link to your project. (Required)'
+        },
+        {
+            type: 'confirm',
+            name: 'feature', 
+            message: 'Would you like to feature this project?',
+            default: false
+        },
+        {
+            type: 'confirm', 
+            name: 'confirmAddProject', 
+            message: 'Would you like to enter another project?',
+            default: false
+        }
+    ])
+    .then(projectData => {
+        portfolioData.projects.push(projectData);
+        if (projectData.confirmAddProject){
+            return promptProject(portfolioData);
+        }
+        else {
+            return portfolioData;
+        }
+    });
+}
+
+// calls the above function, and then logs the user's input as answers on the console. 
+promptUser()
+    .then(promptProject)
+    .then(portfolioData => {
+        console.log(portfolioData);
+    });
